11d94641071e45b40398dcb55d56463a3e88d6a5
Udaykiran87 <uday.patnaik@gmail.com>
2022-06-25 11:40:15 +0900

    added dummy script to be used in future use case

---
+++ src/stage_01_load_save.py
@@ -0,0 +1,55 @@
+import argparse
+import os
+import shutil
+from tqdm import tqdm
+import logging
+from src.utils import read_yaml, create_directories
+import random
+
+
+STAGE = "LoadData" ## <<< change stage name 
+
+logging.basicConfig(
+    filename=os.path.join("logs", 'running_logs.log'), 
+    level=logging.INFO, 
+    format="[%(asctime)s: %(levelname)s: %(module)s]: %(message)s",
+    filemode="a"
+    )
+
+def copy_file(source_download_dir, local_data_dir):
+    list_of_files = os.listdir(source_download_dir)
+    N = len(list_of_files)
+    for file in tqdm(list_of_files, total=N, desc=f'copying file from {source_download_dir} to {local_data_dir}', colour="green"):
+        src = os.path.join(source_download_dir, file)
+        dest = os.path.join(local_data_dir, file)
+        shutil.copy(src, dest)
+
+def main(config_path, params_path):
+    ## read config files
+    config = read_yaml(config_path)
+    params = read_yaml(params_path)
+
+    pass
+    ######Uncomment below code for future usescases and modify as per requirement############
+    # source_download_dirs = config["source_download_dirs"]
+    # local_data_dirs = config["local_data_dirs"]
+
+    # for source_download_dir, local_data_dir in tqdm(zip(source_download_dirs, local_data_dirs), total=1, desc = "list of folders", colour="red"):
+    #     create_directories([local_data_dir])
+    #     copy_file(source_download_dir, local_data_dir)        
+
+
+if __name__ == '__main__':
+    args = argparse.ArgumentParser()
+    args.add_argument("--config", "-c", default="configs/config.yaml")
+    args.add_argument("--params", "-p", default="params.yaml")
+    parsed_args = args.parse_args()
+
+    try:
+        logging.info("\n********************")
+        logging.info(f">>>>> stage {STAGE} started <<<<<")
+        main(config_path=parsed_args.config, params_path=parsed_args.params)
+        logging.info(f">>>>> stage {STAGE} completed!<<<<<\n")
+    except Exception as e:
+        logging.exception(e)
+        raise e
