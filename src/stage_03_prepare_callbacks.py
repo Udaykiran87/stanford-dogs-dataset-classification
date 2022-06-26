ca095b1290e74369495ac11f0b2faed6b87010a9
Udaykiran87 <uday.patnaik@gmail.com>
2022-06-26 14:56:26 +0900

    stage 03 base file ready

--- src/stage_03_prepare_callbacks.py
+++ src/stage_03_prepare_callbacks.py
@@ -6,11 +6,11 @@ import io
 
 
 
-STAGE = "prepare_callback" 
+STAGE = "prepare_callback"
 
 logging.basicConfig(
-    filename=os.path.join("logs", 'running_logs.log'), 
-    level=logging.INFO, 
+    filename=os.path.join("logs", 'running_logs.log'),
+    level=logging.INFO,
     format="[%(asctime)s: %(levelname)s: %(module)s]: %(message)s",
     filemode="a"
     )
@@ -20,7 +20,19 @@ def prepare_callbacks(config_path, params_path):
     ## read config files
     config = read_yaml(config_path)
     params = read_yaml(params_path)
-    pass
+
+    artifacts = config["artifacts"]
+    artifacts_dir = artifacts["ARTIFACTS_DIR"]
+
+    tensorboard_log_dir = os.path.join(artifacts_dir, artifacts["artifacts"]["TENSORBOARD_ROOT_LOG_DIR"])
+    checkpoint_dir = os.path.join(artifacts_dir, artifacts["artifacts"]["CHECKPOINT_DIR"])
+    callbacks_dir = os.path.join(artifacts_dir, artifacts["artifacts"]["CALLBACKS_DIR"])
+
+    create_directories([
+        tensorboard_log_dir,
+        checkpoint_dir,
+        callbacks_dir
+    ])
 
 
 if __name__ == '__main__':
