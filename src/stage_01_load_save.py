c0326042a760fce3ff4be2a4826dfc3365eb2f1d
Udaykiran87 <uday.patnaik@gmail.com>
2022-06-28 06:47:13 +0900

    updated stage 01 to dowbload data

--- src/stage_01_load_save.py
+++ src/stage_01_load_save.py
@@ -4,16 +4,100 @@ import shutil
 from tqdm import tqdm
 import logging
 from src.utils import read_yaml, create_directories
+import zipfile
+import tarfile
+import requests
 
 
-STAGE = "LoadData" ## <<< change stage name 
+STAGE = "LoadData" ## <<< change stage name
+# ARCHIVE EXTENSIONS
+ZIP_EXTENSION = ".zip"
+TAR_EXTENSION = ".tar"
+TAR_GZ_EXTENSION = ".tar.gz"
+TGZ_EXTENSION = ".tgz"
+GZ_EXTENSION = ".gz"
+
+EMPTY_URL_ERROR = "ERROR: URL should not be empty."
+FILENAME_ERROR = "ERROR: Filename should not be empty."
+UNKNOWN_FORMAT = "ERROR: Unknown file format. Can't extract."
 
 logging.basicConfig(
-    filename=os.path.join("logs", 'running_logs.log'), 
-    level=logging.INFO, 
+    filename=os.path.join("logs", 'running_logs.log'),
+    level=logging.INFO,
     format="[%(asctime)s: %(levelname)s: %(module)s]: %(message)s",
     filemode="a"
     )
+
+def download_dataset(url, target_path="data/", keep_download=True, overwrite_download=False):
+    """Downloads dataset from a url.
+    url: string, a dataset path
+    target_path: string, path where data will be downloaded
+    keep_download: boolean, keeps the original file after extraction
+    overwrite_download: boolean, stops download if dataset already exists
+    """
+    if url == "" or url is None:
+        raise Exception(EMPTY_URL_ERROR)
+
+    filename = get_filename(url)
+    file_location = get_file_location(target_path, filename)
+    create_directories([target_path])
+    if os.path.exists(file_location) and not overwrite_download:
+        logging.info(f"File already exists at {file_location}. Use: 'overwrite_download=True' to \
+        overwrite download")
+        extract_file(target_path, filename)
+        return
+    logging.info(f"Downloading file from {url} to {file_location}.")
+    # Download
+    with open(file_location, 'wb') as f:
+        with requests.get(url,allow_redirects=True, stream=True) as resp:
+            for chunk in resp.iter_content(chunk_size = 512):  #chunk_size in bytes
+                if chunk:
+                    f.write(chunk)
+    logging.info("Finished downloading.")
+    logging.info("Extracting the file now ...")
+    extract_file(target_path, filename)
+    if not keep_download:
+        os.remove(file_location)
+
+def extract_file(target_path, filename):
+	"""Extract file based on file extension
+	target_path: string, location where data will be extracted
+	filename: string, name of the file along with extension
+	"""
+	if filename == "" or filename is None:
+		raise Exception(FILENAME_ERROR)
+
+	file_location = get_file_location(target_path, filename)
+
+	if filename.endswith(ZIP_EXTENSION):
+		logging.info("Extracting zip file...")
+		zipf = zipfile.ZipFile(file_location, 'r')
+		zipf.extractall(target_path)
+		zipf.close()
+	elif filename.endswith(TAR_EXTENSION) or \
+		 filename.endswith(TAR_GZ_EXTENSION) or \
+		 filename.endswith(TGZ_EXTENSION):
+		logging.info("Extracting tar file")
+		tarf = tarfile.open(file_location, 'r')
+		tarf.extractall(target_path)
+		tarf.close()
+	elif filename.endswith(GZ_EXTENSION):
+		logging.info("Extracting gz file")
+		out_file = file_location[:-3]
+		with open(file_location, "rb") as f_in:
+			with open(out_file, "wb") as f_out:
+				shutil.copyfileobj(f_in, f_out)
+	else:
+		logging.info(UNKNOWN_FORMAT)
+
+def get_filename(url):
+	"""Extract filename from file url"""
+	filename = os.path.basename(url)
+	return filename
+
+def get_file_location(target_path, filename):
+	""" Concatenate download directory and filename"""
+	return os.path.join(target_path, filename)
 
 def copy_file(source_download_dir, local_data_dir):
     list_of_files = os.listdir(source_download_dir)
@@ -26,14 +110,13 @@ def copy_file(source_download_dir, local_data_dir):
 def get_data(config_path):
     ## read config files
     config = read_yaml(config_path)
-    pass
-    ######Uncomment below code for future usescases and modify as per requirement############
-    # source_download_dirs = config["source_download_dirs"]
-    # local_data_dirs = config["local_data_dirs"]
-
-    # for source_download_dir, local_data_dir in tqdm(zip(source_download_dirs, local_data_dirs), total=1, desc = "list of folders", colour="red"):
-    #     create_directories([local_data_dir])
-    #     copy_file(source_download_dir, local_data_dir)        
+    image_download_url = config["source_download_url"]["image_download_url"]
+    annotation_download_url = config["source_download_url"]["annotation_download_url"]
+    target_path = config["local_data_dirs"]
+    # Download Images
+    download_dataset(url=image_download_url, target_path=target_path)
+    # Download Annotation
+    download_dataset(url=annotation_download_url, target_path=target_path)
 
 
 if __name__ == '__main__':
